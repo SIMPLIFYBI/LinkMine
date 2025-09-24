0833f5708fd6a5204c304a6e6432e1e1a5fc9533
James <james@simplifybi.com>
2025-09-24 20:05:45 +0800

    fix(assets): split client UI from page to allow metadata export

---
+++ app/assets/AssetsClient.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+// If you were using OrgContext hooks/imports, keep them here
+// import { useOrg } from "@/lib/OrgContext";
+
+export default function AssetsClient() {
+  // const { org } = useOrg();
+  return (
+    <div>
+      {/* ...move the former page.jsx client-side UI here... */}
+    </div>
+  );
+}
