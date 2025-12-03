afb8b9e2279c5e7aa0154449b07f01ea1e56b07c
James <james@simplifybi.com>
2025-12-03 18:56:12 +0800

    Header switch

--- app/components/navTabs.js
+++ app/components/navTabs.js
@@ -1,7 +1,7 @@
 const navTabs = [
   { label: "Home", href: "/" },
   { label: "Jobs Board", href: "/jobs" }, // was "Jobs"
-  { label: "Consultants", href: "/consultants" },
+  { label: "Directory", href: "/consultants" },
   { label: "My Activity", href: "/activity" }, // NEW
   { label: "About", href: "/about" },
   { label: "Account", href: "/account" },
